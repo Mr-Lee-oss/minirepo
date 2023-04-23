1923fce5fa615468e6267a9fe86bdea48f08d500
wc <lhd0@naver.com>
2023-04-23 23:44:08 +0900

    첫번째 테스트

--- miniProject/settings.py
+++ miniProject/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accountapp',
 ]
 
 MIDDLEWARE = [
@@ -75,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
--- miniProject/urls.py
+++ miniProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accountapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.account, name='account' )
 ]
